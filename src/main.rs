cb275b58349367d16245ca42bfd02b40e3783046
nadmax <maximilien.nadji@pm.me>
2026-06-09 09:28:54 +0200

    refactor: format code

--- src/main.rs
+++ src/main.rs
@@ -81,10 +81,9 @@ impl Modify for BearerSecurityAddon {
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| {
-                    format!("{}=debug,tower_http=info", env!("CARGO_PKG_NAME")).into()
-                }),
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                format!("{}=debug,tower_http=info", env!("CARGO_PKG_NAME")).into()
+            }),
         )
         .init();
 
